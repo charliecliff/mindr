965aa084012caa3430475695f6a88758223ea5b9
charliecliff <charlie@ridescout.com>
2016-01-30 19:45:49 -0600

    Initial Commit

---
+++ mindr/mindr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mindr
+//
+//  Created by Charles Cliff on 1/30/16.
+//  Copyright © 2016 Charles Cliff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mindr/mindr/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  mindr
+//
+//  Created by Charles Cliff on 1/30/16.
+//  Copyright © 2016 Charles Cliff. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

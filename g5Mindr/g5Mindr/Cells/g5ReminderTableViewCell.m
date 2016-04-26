5e9c09d361cdb640b8d2b4ccfc5e15f3dbdfa62f
charliecliff <charlie@ridescout.com>
2016-04-26 15:10:44 -0500

    Built out the Xib for the Reminder Cell

--- g5Mindr/g5Mindr/Cells/g5ReminderTableViewCell.m
+++ g5Mindr/g5Mindr/Cells/g5ReminderTableViewCell.m
@@ -7,6 +7,17 @@
 //
 
 #import "g5ReminderTableViewCell.h"
+#import "OnSwitchView.h"
+
+@interface g5ReminderTableViewCell ()
+
+@property(nonatomic, strong) IBOutlet UIImageView *iconImageView;
+@property(nonatomic, strong) IBOutlet UILabel *titleLabel;
+@property(nonatomic, strong) IBOutlet UILabel *conditionExplanationLabel;
+@property(nonatomic, strong) IBOutlet OnSwitchView *onSwitch;
+@property(nonatomic, strong) IBOutlet UIButton *onSwitchButton;
+
+@end
 
 @implementation g5ReminderTableViewCell
 
@@ -18,6 +29,23 @@
     [super setSelected:selected animated:animated];
 
     // Configure the view for the selected state
+}
+
+#pragma mark - Actions
+
+- (IBAction)didPressSwitchButton:(id)sender {
+//    BOOL newConditionActiveState = (!self.condition.isActive);
+//    [self.delegate g5Condition:self.condition didSetActive:newConditionActiveState];
+//    [self.condition setIsActive:newConditionActiveState];
+//    
+//    if (self.condition.isActive) {
+//        [self.onSwitch addOFFReversedAnimationWithBeginTime:0 andFillMode:kCAFillModeBoth withDuration:0.200 andRemoveOnCompletion:NO completion:NULL];
+//    }
+//    else {
+//        [self.onSwitch addOFFAnimationWithBeginTime:0 andFillMode:kCAFillModeBoth withDuration:0.200 andRemoveOnCompletion:NO completion:NULL];
+//    }
+//    
+//    [self reload];
 }
 
 @end

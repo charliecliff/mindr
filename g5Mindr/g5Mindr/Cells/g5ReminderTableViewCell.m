67eaf0f951ce332a9f68200640f6adab1d40502c
charliecliff <charlie@ridescout.com>
2016-05-07 21:35:50 -0500

    Emoticon TableView Cells

--- g5Mindr/g5Mindr/Cells/g5ReminderTableViewCell.m
+++ g5Mindr/g5Mindr/Cells/g5ReminderTableViewCell.m
@@ -8,10 +8,13 @@
 
 #import "g5ReminderTableViewCell.h"
 #import "OnSwitchView.h"
+#import "g5ConfigAndMacros.h"
 
 @interface g5ReminderTableViewCell ()
 
-@property(nonatomic, strong) IBOutlet UIImageView *iconImageView;
+@property(nonatomic, strong) IBOutlet UIImageView *outerRingImageView;
+@property(nonatomic, strong) IBOutlet UIImageView *innerRingImageView;
+
 @property(nonatomic, strong) IBOutlet UILabel *emoticonLabel;
 @property(nonatomic, strong) IBOutlet UILabel *titleLabel;
 @property(nonatomic, strong) IBOutlet UILabel *explanationLabel;
@@ -26,11 +29,15 @@
 
 #pragma mark - Configure
 
+
 - (void)configureWithReminder:(g5Reminder *)reminder {
     self.reminder = reminder;
     [self.titleLabel setText:self.reminder.name];
     [self.explanationLabel setText:self.reminder.shortExplanation];
     [self.emoticonLabel setText:self.reminder.emoticonUnicodeCharacter];
+    
+    [self configureOuterRingWithColor:[UIColor whiteColor]];
+    [self configureInnerRingWithColor:PRIMARY_STROKE_COLOR];
     
     if (reminder.isActive) {
         [self.onSwitch addOFFReversedAnimationWithBeginTime:0 andFillMode:kCAFillModeBoth withDuration:0.000 andRemoveOnCompletion:NO completion:NULL];
@@ -39,6 +46,40 @@
     else {
         [self.onSwitch addOFFAnimationWithBeginTime:0 andFillMode:kCAFillModeBoth withDuration:0.000 andRemoveOnCompletion:NO completion:NULL];
     }
+}
+
+- (void)configureOuterRingWithColor:(UIColor *)color {
+    CGFloat radius = self.outerRingImageView.frame.size.width/2;
+    CGPoint center = CGPointMake(self.outerRingImageView.frame.size.width, self.outerRingImageView.frame.size.height);
+    
+    CAShapeLayer *circleLayer = [CAShapeLayer layer];
+    [circleLayer setBounds:CGRectMake(0.0f, 0.0f, [self.outerRingImageView bounds].size.width, [self.outerRingImageView bounds].size.height)];
+    [circleLayer setPosition:CGPointMake(0,0)];
+    
+    UIBezierPath *path = [UIBezierPath bezierPathWithArcCenter:center radius:radius startAngle:0 endAngle:2*M_PI clockwise:YES];
+    
+    [circleLayer setPath:[path CGPath]];
+    [circleLayer setFillColor:[color CGColor]];
+    [circleLayer setLineWidth:0.0f];
+    
+    [[self.outerRingImageView layer] addSublayer:circleLayer];
+}
+
+- (void)configureInnerRingWithColor:(UIColor *)color {
+    CGFloat radius = self.innerRingImageView.frame.size.width/2;
+    CGPoint center = CGPointMake(self.innerRingImageView.frame.size.width, self.innerRingImageView.frame.size.height);
+    
+    CAShapeLayer *circleLayer = [CAShapeLayer layer];
+    [circleLayer setBounds:CGRectMake(0.0f, 0.0f, [self.innerRingImageView bounds].size.width, [self.innerRingImageView bounds].size.height)];
+    [circleLayer setPosition:CGPointMake(0,0)];
+    
+    UIBezierPath *path = [UIBezierPath bezierPathWithArcCenter:center radius:radius startAngle:0 endAngle:2*M_PI clockwise:YES];
+    
+    [circleLayer setPath:[path CGPath]];
+    [circleLayer setFillColor:[color CGColor]];
+    [circleLayer setLineWidth:0.0f];
+    
+    [[self.innerRingImageView layer] addSublayer:circleLayer];
 }
 
 - (void)reload {

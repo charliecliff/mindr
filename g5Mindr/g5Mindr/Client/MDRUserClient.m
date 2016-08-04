bd3bbcd692f03d080e54eef15e6cb7e6630e64e2
charliecliff <charlescliff@Charless-MacBook-Pro.local>
2016-08-04 09:03:27 -0500

    Allowing the lines on the conditions labels to wrap

--- g5Mindr/g5Mindr/Client/MDRUserClient.m
+++ g5Mindr/g5Mindr/Client/MDRUserClient.m
@@ -5,26 +5,26 @@
 @implementation MDRUserClient
 
 + (void)postContextForUserContext:(MDRUserContext *)context withSuccess:(void (^)(void))success withFailure:(void (^)(void))failure {
-    NSString *requestString = @"http://ec2-54-149-60-145.us-west-2.compute.amazonaws.com:8000/context";
-    NSString *escapedString = [requestString stringByAddingPercentEncodingWithAllowedCharacters:NSCharacterSet.URLQueryAllowedCharacterSet];
-    
-    NSDictionary *paramaters = [context encodeToDictionary];
-    
-    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
-    manager.requestSerializer  = [AFJSONRequestSerializer serializer];
-    manager.responseSerializer = [AFJSONResponseSerializer serializer];
-    
-    [manager POST:escapedString parameters:paramaters success:^(AFHTTPRequestOperation *operation, id responseObject) {
-        NSLog(@"Success");
-        if (success) {
-            success();
-        }
-    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-        NSLog(@"Failure");
-        if (failure) {
-            failure();
-        }
-    }];
+//    NSString *requestString = @"http://ec2-54-149-60-145.us-west-2.compute.amazonaws.com:8000/context";
+//    NSString *escapedString = [requestString stringByAddingPercentEncodingWithAllowedCharacters:NSCharacterSet.URLQueryAllowedCharacterSet];
+//    
+//    NSDictionary *paramaters = [context encodeToDictionary];
+//    
+//    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+//    manager.requestSerializer  = [AFJSONRequestSerializer serializer];
+//    manager.responseSerializer = [AFJSONResponseSerializer serializer];
+//    
+//    [manager POST:escapedString parameters:paramaters success:^(AFHTTPRequestOperation *operation, id responseObject) {
+//        NSLog(@"Success");
+//        if (success) {
+//            success();
+//        }
+//    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+//        NSLog(@"Failure");
+//        if (failure) {
+//            failure();
+//        }
+//    }];
 }
 
 @end

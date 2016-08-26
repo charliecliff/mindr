53b6cce207be32364ff10a8c04c5c3a6142950ba
charliecliff <charlescliff@Charless-MBP.attlocal.net>
2016-08-26 12:56:56 -0500

    Removed the Alert Sounds

--- g5Mindr/g5Mindr/ViewControllers/MDRReminderViewController.m
+++ g5Mindr/g5Mindr/ViewControllers/MDRReminderViewController.m
@@ -36,7 +36,8 @@ static NSString *MDRReminderDetailCellIdentifier = @"reminder_detail_cell";
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 3;
+    return 2;
+//    return 3;
 }
 
 @end

142d086ea192e4f261ff87d4ed64890a143da9ba
ccliff <charlie.cliff@gmail.com>
2016-09-26 14:00:28 -0500

    Getting Push Notifications

--- g5Mindr/g5Mindr/AppDelegate.m
+++ g5Mindr/g5Mindr/AppDelegate.m
@@ -20,7 +20,8 @@
 
 @implementation AppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+- (BOOL)application:(UIApplication *)application
+  didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
     //  Handle launching from a notification
     UILocalNotification *locationNotification = [launchOptions objectForKey:UIApplicationLaunchOptionsLocalNotificationKey];
@@ -43,16 +44,19 @@
     [[g5ReminderManager sharedManager] loadReminders];
     [[MDRLocationManager sharedManager] startUpdatingLocation];
     [[MDRUserManager sharedManager] bindToLocationManager:[MDRLocationManager sharedManager]];
-    
+  
+    NSString *userID = [MDRUserManager sharedManager].currentUserContext.userID;
     //  Register For Push Notifications
-    if ([MDRUserManager sharedManager].currentUserContext.userID == nil || [[MDRUserManager sharedManager].currentUserContext.userID isEqualToString:@"simulator_id"]) {
+    if (userID == nil || [userID isEqualToString:@"simulator_id"]){
         [self registerForPushNotifications];
     }
     
     //  Root View Controller
     UIStoryboard *sbReminderList = [UIStoryboard storyboardWithName:@"MDRReminderList" bundle:nil];
     MDRReminderListViewController *vc = [sbReminderList instantiateInitialViewController];
-    mindrBounceNavigationViewController *bounceVC = [[mindrBounceNavigationViewController alloc] initWithRootViewController:vc withDelegate:vc withDatasource:nil];
+    mindrBounceNavigationViewController *bounceVC = [[mindrBounceNavigationViewController alloc] initWithRootViewController:vc
+                                                                                                               withDelegate:vc
+                                                                                                             withDatasource:nil];
     bounceVC.datasource = bounceVC;
     
     vc.bounceNavigationController = bounceVC;
@@ -74,33 +78,43 @@
 
 #pragma mark - Push Notifications
 
-- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfon fetchCompletionHandler:(void (^)(UIBackgroundFetchResult result))handler {
+- (void)application:(UIApplication *)application
+  didReceiveRemoteNotification:(NSDictionary *)userInfon
+  fetchCompletionHandler:(void (^)(UIBackgroundFetchResult result))handler {
     
-    
-    [UIApplication sharedApplication].applicationIconBadgeNumber = [UIApplication sharedApplication].applicationIconBadgeNumber + 1 ;
+    NSInteger badgeNumber = [UIApplication sharedApplication].applicationIconBadgeNumber + 1 ;
+    [UIApplication sharedApplication].applicationIconBadgeNumber = badgeNumber;
 }
 
 
 - (void)registerForPushNotifications {
-    UIUserNotificationType types = (UIUserNotificationType) (UIUserNotificationTypeBadge | UIUserNotificationTypeSound | UIUserNotificationTypeAlert);
-    UIUserNotificationSettings *mySettings = [UIUserNotificationSettings settingsForTypes:types categories:nil];
+    UIUserNotificationType types = (UIUserNotificationType) (UIUserNotificationTypeBadge |
+                                                             UIUserNotificationTypeSound |
+                                                             UIUserNotificationTypeAlert);
+    UIUserNotificationSettings *mySettings = [UIUserNotificationSettings settingsForTypes:types
+                                                                               categories:nil];
     [[UIApplication sharedApplication] registerUserNotificationSettings:mySettings];
 }
 
-- (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(nonnull UIUserNotificationSettings *)notificationSettings {
-    if ([[UIApplication sharedApplication] respondsToSelector:@selector(registerForRemoteNotifications)]) {
-        [[UIApplication sharedApplication] registerForRemoteNotifications];
+- (void)application:(UIApplication *)application
+  didRegisterUserNotificationSettings:(nonnull UIUserNotificationSettings *)notificationSettings {
+    UIApplication *app = [UIApplication sharedApplication];
+    BOOL test = [app respondsToSelector:@selector(registerForRemoteNotifications)];
+    if (test) {
+      [[UIApplication sharedApplication] registerForRemoteNotifications];
     }
 }
 
-- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+- (void)application:(UIApplication *)application
+  didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
     NSString *token = [[deviceToken description] stringByTrimmingCharactersInSet: [NSCharacterSet characterSetWithCharactersInString:@"<>"]];
     token = [token stringByReplacingOccurrencesOfString:@" " withString:@""];
     [[MDRUserManager sharedManager] setUserID:token];
     [[MDRUserManager sharedManager] updateContext];
 }
 
-- (void)application:(UIApplication *)app didFailToRegisterForRemoteNotificationsWithError:(NSError *)err {
+- (void)application:(UIApplication *)app
+  didFailToRegisterForRemoteNotificationsWithError:(NSError *)err {
     NSLog(@"Error in registration. Error: %@", err);
 }
 

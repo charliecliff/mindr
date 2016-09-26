2195366d293e2c1eb603f65762ca84bbd5858301
ccliff <charlie.cliff@gmail.com>
2016-09-26 16:41:15 -0500

    Adding HTML Animations

--- g5Mindr/g5Mindr/Models/User/MDRUserContext.h
+++ g5Mindr/g5Mindr/Models/User/MDRUserContext.h
@@ -1,3 +1,11 @@
+//
+//  MDRUserContext.h
+//  g5Mindr
+//
+//  Created by Charles Cliff on 3/19/16.
+//  Copyright © 2016 Charles Cliff. All rights reserved.
+//
+
 #import <Foundation/Foundation.h>
 
 @class CLLocation;
--- g5Mindr/g5Mindr/ViewControllers/MDREmptyViewController.m
+++ g5Mindr/g5Mindr/ViewControllers/MDREmptyViewController.m
@@ -12,9 +12,10 @@
 static NSString *const fancyQuote               = @"\"It is not the ship so much as the skillful sailing that assures the prosperous voyage.\"";
 static NSString *const fancyQuoteHighlightedPart = @"skillful sailing";
 
-@interface MDREmptyViewController ()
+@interface MDREmptyViewController () <UIWebViewDelegate>
 
 @property(nonatomic, strong) IBOutlet UILabel *fancyQuoteLabel;
+@property (nonatomic, strong) IBOutlet UIWebView *animationWebView;
 
 @end
 
@@ -23,6 +24,11 @@ static NSString *const fancyQuoteHighlightedPart = @"skillful sailing";
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self buildFancyQuote];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+  [super viewWillAppear:animated];
+  [self setupAnimationWebView];
 }
 
 - (void)buildFancyQuote {
@@ -41,6 +47,48 @@ static NSString *const fancyQuoteHighlightedPart = @"skillful sailing";
     [attString addAttribute:NSForegroundColorAttributeName value:GOLD_COLOR range:range];
    
     self.fancyQuoteLabel.attributedText = attString;
+}
+
+#pragma mark - Animation
+
+- (void)setupAnimationWebView {
+  NSString *path = [[NSBundle mainBundle] pathForResource:@"buoy_logo_anim"
+                                                   ofType:@"html"];
+  NSURL *url = [NSURL fileURLWithPath:path];
+  [self.animationWebView loadRequest:[NSURLRequest requestWithURL:url]];
+  self.animationWebView.delegate = self;
+  
+  self.animationWebView.scrollView.scrollEnabled = NO;
+  self.animationWebView.scalesPageToFit = YES;
+  self.animationWebView.contentMode = UIViewContentModeScaleAspectFit;
+
+  self.animationWebView.userInteractionEnabled = NO;
+}
+
+#pragma mark - UIWebViewDelegate
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
+  
+  NSLog(@"success");
+//  self.webViewDoneLoading = YES;
+//  
+//  if (self.loadAnimation) {
+    [self.animationWebView stringByEvaluatingJavaScriptFromString:@"newAnimation(seq1);"];
+//    [UIView animateWithDuration:0.3 animations:^{
+//      self.loadingAnimation.alpha = 1.0;
+//      self.loadingRidesLabel.alpha = 1.0;
+//    }];
+//  }
+//  else {
+//    if (self.completion) {
+//      [self animationModeChange:self.completion];
+//    }
+//  }
+}
+
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
+  
+  NSLog(@"Error : %@",error);
 }
 
 @end

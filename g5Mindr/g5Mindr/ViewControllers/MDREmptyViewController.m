2f0cce66505c15a1d2c468b5ee2cf8a2f5a54635
ccliff <charlie.cliff@gmail.com>
2016-09-27 10:43:28 -0500

    Transitioning to the Animation

--- g5Mindr/g5Mindr/ViewControllers/MDREmptyViewController.m
+++ g5Mindr/g5Mindr/ViewControllers/MDREmptyViewController.m
@@ -14,80 +14,47 @@ static NSString *const fancyQuoteHighlightedPart = @"skillful sailing";
 
 @interface MDREmptyViewController () <UIWebViewDelegate>
 
-@property(nonatomic, strong) IBOutlet UILabel *fancyQuoteLabel;
+@property (nonatomic, strong) IBOutlet UIImageView *logoImageView;
 @property (nonatomic, strong) IBOutlet UIWebView *animationWebView;
 
 @end
 
 @implementation MDREmptyViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    [self buildFancyQuote];
-}
-
 - (void)viewWillAppear:(BOOL)animated {
   [super viewWillAppear:animated];
   [self setupAnimationWebView];
 }
 
-- (void)buildFancyQuote {
-        
-    UIFont *systemFont = [UIFont fontWithName:@"ProximaNovaSoftW03-Semibold" size:25];
-    NSDictionary * fontAttributes = [[NSDictionary alloc] initWithObjectsAndKeys:systemFont, NSFontAttributeName, nil];
-    NSMutableAttributedString *attString = [[NSMutableAttributedString alloc] initWithString:fancyQuote attributes:fontAttributes];
-
-    [attString addAttribute:NSForegroundColorAttributeName value:[UIColor whiteColor] range:NSMakeRange(0, attString.length)];
-
-    NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
-    [paragraphStyle setAlignment:NSTextAlignmentCenter];
-    [attString addAttribute:NSParagraphStyleAttributeName value:paragraphStyle range:NSMakeRange(0, attString.length)];
-
-    NSRange range = [fancyQuote rangeOfString:fancyQuoteHighlightedPart];
-    [attString addAttribute:NSForegroundColorAttributeName value:GOLD_COLOR range:range];
-   
-    self.fancyQuoteLabel.attributedText = attString;
-}
-
 #pragma mark - Animation
 
 - (void)setupAnimationWebView {
-  NSString *path = [[NSBundle mainBundle] pathForResource:@"buoy_logo_anim"
-                                                   ofType:@"html"];
+  NSString *path = [[NSBundle mainBundle] pathForResource:@"buoy_logo_anim" ofType:@"html"];
   NSURL *url = [NSURL fileURLWithPath:path];
   [self.animationWebView loadRequest:[NSURLRequest requestWithURL:url]];
   self.animationWebView.delegate = self;
-  
   self.animationWebView.scrollView.scrollEnabled = NO;
   self.animationWebView.scalesPageToFit = YES;
   self.animationWebView.contentMode = UIViewContentModeScaleAspectFit;
-
   self.animationWebView.userInteractionEnabled = NO;
+  self.animationWebView.alpha = 0.0;
 }
 
 #pragma mark - UIWebViewDelegate
 
 - (void)webViewDidFinishLoad:(UIWebView *)webView {
   
-  NSLog(@"success");
-//  self.webViewDoneLoading = YES;
-//  
-//  if (self.loadAnimation) {
+  [UIView animateWithDuration:0.3
+                   animations:^{
+    self.animationWebView.alpha = 1.0;
+                   }
+                   completion:^(BOOL finished) {
     [self.animationWebView stringByEvaluatingJavaScriptFromString:@"newAnimation(seq1);"];
-//    [UIView animateWithDuration:0.3 animations:^{
-//      self.loadingAnimation.alpha = 1.0;
-//      self.loadingRidesLabel.alpha = 1.0;
-//    }];
-//  }
-//  else {
-//    if (self.completion) {
-//      [self animationModeChange:self.completion];
-//    }
-//  }
+
+                   }];
 }
 
 - (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
-  
   NSLog(@"Error : %@",error);
 }
 
